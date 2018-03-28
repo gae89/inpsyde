3eefabf99d9c443fe1a6a6625f3cbb54ee7fd7c7
gae89 <gaetanodaquila@hotmail.com>
2018-03-28 23:11:29 +0200

    Update myplugin.php

--- tests/myplugin.php
+++ tests/myplugin.php
@@ -1,9 +1,9 @@
 <?php
-
+//Take the code...
 $nonce = $_GET['_wpnonce'];
-
+//...and verify it.
 if ( wp_verify_nonce( $nonce, 'say-hello' ) ){
-
+    //The code is corret, so...
     echo 'Hellooo!!!';
 } else {
     die( 'Good bye!' );
